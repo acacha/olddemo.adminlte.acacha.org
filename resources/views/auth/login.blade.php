f0d549c92069cc2fdb430a2c8354c0a10c01743f
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-01-03 15:41:23 +0100

    Added info for default user in logging page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,14 +24,15 @@
 
     <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
+        <p class="login-box-msg">Default user: adminlte@acacha.org <br/> Password: adminlte <br/>or you can create your own user in <a href="{{ url('/register') }}" class="text-center">register</a> page</p>
     <form action="{{ url('/login') }}" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group has-feedback">
-            <input type="email" class="form-control" placeholder="Email" name="email"/>
+            <input type="email" class="form-control" placeholder="Email" name="email" value="adminlte@acacha.org"/>
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password" name="password"/>
+            <input type="password" class="form-control" placeholder="Password" name="password" value="adminlte"/>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
